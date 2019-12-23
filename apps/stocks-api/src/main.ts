f37fd1f96f73dc77be2a273ae39d1f0466f38ed0
Naga Rani Tumu <NagaRani.Tumu2@T-Mobile.com>
2019-12-23 17:13:53 +0530

    'task4changes'

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -4,6 +4,36 @@
  **/
 import { Server } from 'hapi';
 
+import redis from 'redis';
+import { environment } from './environments/environment';
+import * as request from 'request';
+
+const getData = (client, key) => {
+  return new Promise((resolve, reject) => {
+    client.get(key, (err, result) => {
+      if (err || !result) {
+        resolve(null);
+      } else {
+        resolve(JSON.parse(result));
+      }
+    })
+  });
+}
+
+
+const fetchData = (symbol, period) => {
+  return new Promise((resolve) => {
+    request
+      .get(`${environment.apiURL}/beta/stock/${symbol}/chart/${period
+        }?token=${environment.apiKey}`, function (error, response, body) {
+          resolve(JSON.parse(body));
+        })
+  });
+}
+
+const postData = (client, key, value) => {
+  client.setex(key, 3600, JSON.stringify(value));
+}
 const init = async () => {
   const server = new Server({
     port: 3333,
@@ -17,6 +47,34 @@ const init = async () => {
       return {
         hello: 'world'
       };
+    }
+  });
+
+  const redisClient = redis.createClient(
+    {
+      host: 'localhost',
+      port: 6379,
+    }
+  );
+  redisClient.on("error", function (err) {
+    console.error("Redis error.", err);
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/sandbox',
+    handler: async (request, h) => {
+      if (request.query.symbol && request.query.period) {
+        const urlstring = `${request.query.symbol}${request.query.period}`;
+        const stocks = await getData(redisClient, urlstring);
+        if (!stocks) {
+          const response = await fetchData(request.query.symbol, request.query.period);
+          postData(redisClient, urlstring, response);
+          return response;
+        }
+        return stocks;
+      }
+      return { msg: 'Invalid query parameters' };
     }
   });
 
